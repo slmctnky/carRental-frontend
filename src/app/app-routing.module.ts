09ccca489082f710ab34a75e49185fb3b836bd6a
Salim Çetin <salimcetinkaya@outlook.com>
2021-03-16 13:07:10 +0300

    17.Hafta Ödev Sonu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarComponent } from './components/car/car.component';
 import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
 import { RentalComponent } from './components/rental/rental.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full", component:CarComponent},
   {path:"cars",component:CarComponent},
   {path:"rentals",component:RentalComponent},
-  {path:"colors",component:ColorComponent}
+  {path:"colors",component:ColorComponent},
+  {path:"customers",component:CustomerComponent}
 ];
 
 @NgModule({
